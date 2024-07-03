6b536ef1d700de4277a11081989c50966d28bb52
GuhanGK <guhankumar656@gmail.com>
2024-07-03 12:03:44 +0530

    main
    setup commit 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './Login';
-import { SignUp } from './Signup';
+import "./App.css";
+import Login from "./Login";
+import { SignUp } from "./Signup";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      <SignUp />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<SignUp />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
